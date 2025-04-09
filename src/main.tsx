5b4f7e987bf9aaf739802a68faa154a95aa6a366
Archie G <acgelonga@gmail.com>
2025-04-08 20:59:03 -0500

    Publish again

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 
-import { TuxTheme } from "./theme.ts";
+import { TuxTheme } from "./theme";
 import { ThemeProvider } from "@mui/material";
 
 createRoot(document.getElementById("root")!).render(
